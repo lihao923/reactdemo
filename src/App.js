13575830f996dc85c6ac5fc87dc6da8c72e3cc92
unknown <lihao923@126.com>
2019-07-03 15:32:39 +0800

    add new files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/css/App.css';
+
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>这是App组件！</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
